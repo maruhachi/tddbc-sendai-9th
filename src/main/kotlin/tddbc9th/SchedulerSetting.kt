699a4cf872eccdd99837f2799196b3ca25a0a76e
maruhachi <konchan1129@gmail.com>
2019-11-23 19:52:29 +0900

    各時刻の持ち方を文字列へ。毎時の考え方を仮で。

--- src/main/kotlin/tddbc9th/SchedulerSetting.kt
+++ src/main/kotlin/tddbc9th/SchedulerSetting.kt
@@ -2,5 +2,8 @@ package tddbc9th
 
 class SchedulerSetting(val time: Time) {
     fun getString() = "${time.second} ${time.minute} ${time.hour}"
-    fun isTimeMatch(opponentTime: Time) = time.isMatch(opponentTime)
+    fun isTimeMatch(opponentTime: Time): Boolean{
+        return time.isMatch(opponentTime)
+
+    }
 }
--- src/test/kotlin/tddbc9th/SchedulerSettingSpec.kt
+++ src/test/kotlin/tddbc9th/SchedulerSettingSpec.kt
@@ -8,16 +8,25 @@ object SchedulerSettingSpec : Spek({
     describe("スケジューラー設定クラス") {
         // 0,0,0は初期化のための無意味な値
         var time = Time("18", "9", "32")
+        // TODO: なにも持たない共通interfaceを作るとこの冗長な初期化は不要
         var schedulerSetting = SchedulerSetting(Time("0", "0", "0"))
         beforeEachTest {
             schedulerSetting = SchedulerSetting(time)
         }
 
-        describe("スケジューラー設定オブジェクトの時刻の文字列表現を取得") {
-            it("18時9分32秒の文字列表現「32 9 18」を取得") {
-                Assertions.assertEquals("32 9 18", schedulerSetting.getString())
+        // KotlinらしいParameterizedTest
+        listOf(
+            listOf("32 9 18", Time("18", "9", "32")),
+            listOf("59 59 23", Time("23", "59", "59"))
+        ).forEach { testParam ->
+            describe("スケジューラー設定オブジェクトの時刻の文字列表現を取得") {
+                it("18時9分32秒の文字列表現「32 9 18」を取得") {
+                    schedulerSetting = SchedulerSetting(testParam[1] as Time)
+                    Assertions.assertEquals(testParam[0], schedulerSetting.getString())
+                }
             }
         }
+
 
         describe("時刻が一致するか判断") {
             it("スケジューラ設定「32 9 18」と実行時刻18時9分32秒が一致する") {
@@ -37,7 +46,9 @@ object SchedulerSettingSpec : Spek({
         }
         describe("秒を*にしたとき、任意の時・分の0〜59秒の範囲で一致する") {
             it("スケジューラ設定「* 9 18」と実行時刻18時9分0秒が一致する") {
+                // TODO: 渡すのは*じゃなくて毎秒 という日本語
                 val schedulerSetting = SchedulerSetting(Time("18", "9", "*"))
+                // TODO: 名前の付け方。書いたときに英語として語順がわかりやすい名前
                 Assertions.assertTrue(schedulerSetting.isTimeMatch(Time("18", "9", "0")))
             }
         }
