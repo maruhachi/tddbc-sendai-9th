8ed2c69a98cea5d5cb1a4051bf561501f9052d98
maruhachi <konchan1129@gmail.com>
2019-11-23 14:52:32 +0900

    時刻オブジェクトへ切り出した

---
+++ src/main/kotlin/tddbc9th/Time.kt
@@ -0,0 +1,4 @@
+package tddbc9th
+
+class Time(val hour: Int, val minute: Int, val second: Int) {
+}
--- src/test/kotlin/tddbc9th/TimeSpec.kt
+++ src/test/kotlin/tddbc9th/TimeSpec.kt
@@ -1,6 +1,23 @@
 package tddbc9th
 
+import org.junit.jupiter.api.Assertions
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
 
 object TimeSpec : Spek({
+    describe("時刻クラス"){
+        var time= Time(0,0,0)
+        beforeEachTest {
+            time = Time(18, 9, 32)
+        }
+        it("18時9分32秒を与えると時として18を返す"){
+            Assertions.assertEquals(time.hour , 18)
+        }
+        it("18時9分32秒を与えると分として9を返す"){
+            Assertions.assertEquals(time.minute , 9)
+        }
+        it("18時9分32秒を与えると秒として32を返す"){
+            Assertions.assertEquals(time.second , 32)
+        }
+    }
 })
