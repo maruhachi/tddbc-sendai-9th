5ee535f225363aee462446472f0921a4d173ddbd
ykonno <konchan1129@gmail.com>
2019-11-25 17:14:08 +0900

    change jdk version 1.8 to 11.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib"))
     testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.8")
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.8")
     testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.3.60")
@@ -20,7 +20,7 @@ dependencies {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
 
 tasks.withType<Test> {
